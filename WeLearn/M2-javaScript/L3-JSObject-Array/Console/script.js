054883f73c04b4e70cad856c25b9c981fe0e8d58
Nathan Paz <paz017@csusm.edu>
2019-07-25 10:33:09 -0700

    Day 4: 10:32pm: July 25: Completed Challenge

---
+++ WeLearn/M2-javaScript/L3-JSObject-Array/Console/script.js
@@ -0,0 +1,17 @@
+const article = {
+  name:'Dog family gives birth to 10 punnies',
+  views: 1035,
+  datePublished: '03/25/2018',
+  author: {
+    name: 'Nathan Paz',
+    title: 'Senior Canine Editor'
+  }
+};
+
+
+const topTenFoods = {
+  1: 'Noodles-and-chow-mein-chicken',
+  2: 'Pizza-and-pesto-spaggati',
+  3: 'Burritos',
+  4: '',
+};
