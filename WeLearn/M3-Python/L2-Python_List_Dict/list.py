500208f1ac9afb7ff9e22ccf5320da5ec5281a89
Nathan Paz <paz017@csusm.edu>
2019-08-01 09:13:58 -0700

    Day 8

--- WeLearn/M3-Python/L2-Python_List_Dict/list.py
+++ WeLearn/M3-Python/L2-Python_List_Dict/list.py
@@ -24,7 +24,7 @@
 # for index in range(len(smith_siblings)):
 #     smith_siblings[index] = smith_siblings + "Smith"
 # print(smith_siblings)
-
+#
 # superheros = ["Captain Marvel" , "Wonder Woman" , "Storm" , "Kamala Khan" , "Bumblebee" , "Jessica Jones"]
 #
 # demoted_hero = str(raw_input("What superhero do you want to eliminate from the top 5"))
@@ -34,11 +34,33 @@
 #     print("Top 5 heroes:" , superheros)
 # else:
 #     print("Hero not found.")
+#
+# names = ["Rickon" , "Bran" , "Arya" , "Sansa" , "Jon" , "Robb"]
+# names[::-1]
+# names[4:2:-1]
+# print(names[::-1])
+# print(names[4:2:-1])
+# print(names[:2])
+# print(names[::2])
 
-names = ["Rickon" , "Bran" , "Arya" , "Sansa" , "Jon" , "Robb"]
-names[::-1]
-names[4:2:-1]
-print(names[::-1])
-print(names[4:2:-1])
-print(names[:2])
-print(names[::2])
+state1 = 'New York'
+abbv1 = 'NY'
+state2 = 'California'
+abbv2 = 'CA'
+state3 = 'Texas'
+abbv3 = 'TX'
+print(abbv2, " is short for " , state2)
+
+labeled states = ['NY: New York' , 'CA: California' , 'TX: Texas']
+print(labeled_states[1])
+
+states = ['New York' , 'California' , 'Texas']
+abbvs = ['NY' , 'CA' , 'TX']
+print(abbvs[1] + "is short for" + states[1])
+
+states = {'NY': 'New York' , 'CA': 'California' , 'TX': 'Texas'}
+states = {
+    'NY' : 'New York'
+    'CA' : 'California'
+    'TX' : 'Texas'
+}
